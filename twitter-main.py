1a0415e0ecfb8449a9d12ed0bf0ad3d0a6ea565b
Enrique Enciso <encisoenrique@gmail.com>
2017-04-19 13:44:40 -0500

    added corrected datatype in indico key

--- twitter-main.py
+++ twitter-main.py
@@ -12,12 +12,14 @@ from getTweets import get_tweets
 from getSearch import get_hash_tags
 from indico import get_text_analysis
 
-indicoio.config.api_key = os.environ['INDICO_KEY']
 
+indico_key = os.environ['INDICO_KEY']
 consumer_key = os.environ['TWITTER_KEY']
 consumer_secret = os.environ['TWITTER_SECRET']
 access_token = os.environ['TWITTER_TOKEN']
 access_token_secret = os.environ['TWITTER_TOKEN_SECRET']
+
+indicoio.config.api_key = indico_key
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
