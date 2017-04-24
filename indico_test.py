73bca991b979d2d2e07e7180c87c53e4517b7185
valenc3x <ricardoivan@outlook.com>
2017-04-24 14:24:13 -0500

    removed indico simple test script

--- indico_test.py
+++
@@ -1,7 +0,0 @@
-import indicoio
-indicoio.config.api_key = '98b26a72259df5f8df1f280747a6f6d6'
-
-text = "¿Qué hicieron los candidatos al Gobierno del Estado de México para combatir la pobreza? ¡Compara y decide!…"
-# single example
-x = indicoio.analyze_text(text, apis=['sentiment',  'political', 'keywords','text_tags' ])
-print x
